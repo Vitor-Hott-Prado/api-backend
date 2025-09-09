d449a22d69d6974b70bec4a8e3764cd16b6c97c4
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-09-09 13:35:39 -0300

    09.09.25

---
+++ src/products/products.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ProductsController,  } from './products.controller';
+import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/prduct.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  controllers: [ProductsController],
+  providers: [ProductsService]
+})
+export class ProductsModule {}
+
+
